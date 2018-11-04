a774d5a1cce1b9061cbc816a25ee51831708a304
Vaibhav <vaibhav.verma2697@gmail.com>
2018-11-04 22:45:47 +0530

    fixed to show only images in upload

--- src/js/components/FileUploadButtons.js
+++ src/js/components/FileUploadButtons.js
@@ -8,13 +8,13 @@ export default props =>
             <label htmlFor = 'single'>
                 <FontAwesomeIcon icon={faImage} color='#3B5998' style={{height:'20vh',width:'20vw',cursor:'pointer'}} />
             </label>
-            <input type='file'className="d-none" id='single' onChange={props.onChange} /> 
+            <input type='file' accept="image/*" className="d-none" id='single' onChange={props.onChange} /> 
         </div>
         
         <div className = 'button d-inline-block m-2 ml-5 d-inline-block'>
             <label htmlFor = 'multi'>
                 <FontAwesomeIcon icon={faImages} color='#6d84b4' style={{height:'20vh',width:'20vw',cursor:'pointer'}} />
             </label>
-            <input className="d-none" type='file' id='multi' onChange={props.onChange} multiple  />
+            <input className="d-none" accept="image/*" type='file' id='multi' onChange={props.onChange} multiple  />
         </div>
     </div>
