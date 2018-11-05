228046c18f2e46a38fa56612902c9d08173031d5
Vaibhav <vaibhav.verma2697@gmail.com>
2018-11-05 21:44:22 +0530

    added swipe  gestures for mobile version

--- src/js/components/ImageModal.js
+++ src/js/components/ImageModal.js
@@ -1,11 +1,23 @@
 import React,{Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft,faChevronRight,faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import ReactTouchEvents from "react-touch-events";
 
 class ImageModal extends Component{
     constructor(){
         super();
+        this.handleSwipe = this.handleSwipe.bind(this);
+    }    
+    
+    handleSwipe(direction){
+        switch(direction){
+            case "top":
+            case "left": this.props.nextImage();break;
+            case "bottom":
+            case "right": this.props.previousImage();break;
+        }                
     }
+
     render(){
         return(
             <div className = "position-fixed text-green container-fluid" style = {{top:'0',background:'rgba(200,200,200,0.9'}}>
@@ -18,29 +30,35 @@ class ImageModal extends Component{
                     onClick = {this.props.closeModal}
                 /> 
 
-                <div className = "row" >
-                    <div className = "col-1 d-flex align-items-center p-0" onClick = { this.props.previousImage }>
-                        <FontAwesomeIcon
-                                icon = { faChevronLeft }
-                                color='#3B5998'
-                                style={{cursor:'pointer',height:'10vh',width:'100%'}}
-                        />
+                <ReactTouchEvents
+                    onTap = { this.handleTap }
+                    onSwipe = { this.handleSwipe }
+                >
+                    <div className = "row" >
+                        <div className = "col-lg-1 d-none d-lg-flex align-items-center p-0" onClick = { this.props.previousImage }>
+                            <FontAwesomeIcon
+                                    icon = { faChevronLeft }
+                                    color='#3B5998'
+                                    style={{cursor:'pointer',height:'10vh',width:'100%'}}
+                            />
+                        </div>                                        
+                        <div className = "col-12 col-lg-10 d-flex align-items-center justify-content-center p-0" style = {{height:'100vh'}}>
+                            <img 
+                                src = { this.props.activeImageUrl }
+                                alt = "Yeah not loading" 
+                                style={{maxHeight:'100vh',maxWidth:'100%'}} 
+                            />
+                        </div>                    
+                        
+                        <div className = "col-lg-1 d-none d-lg-flex align-items-center p-0" onClick = { this.props.nextImage }>
+                            <FontAwesomeIcon
+                                    icon = { faChevronRight }
+                                    color='#3B5998'
+                                    style={{cursor:'pointer',height:'10vh',width:'100%'}}
+                            />
+                        </div>
                     </div>
-                    <div className = "col-10 d-flex align-items-center justify-content-center p-0" style = {{height:'100vh'}}>
-                        <img 
-                            src = { this.props.activeImageUrl }
-                            alt = "Yeah not loading" 
-                            style={{maxHeight:'100vh',maxWidth:'100%'}} 
-                        />
-                    </div>
-                    <div className = "col-1 d-flex align-items-center p-0" onClick = { this.props.nextImage }>
-                        <FontAwesomeIcon
-                                icon = { faChevronRight }
-                                color='#3B5998'
-                                style={{cursor:'pointer',height:'10vh',width:'100%'}}
-                        />
-                    </div>
-                </div>                
+                </ReactTouchEvents>
             </div>
         )
     }
