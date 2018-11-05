5acaa3b7e3c1a4bbc1f10c5e335179aeb290e673
Vaibhav <vaibhav.verma2697@gmail.com>
2018-11-05 12:01:07 +0530

    added image modal in ImagesGallery

--- src/js/components/ImagesGallery.js
+++ src/js/components/ImagesGallery.js
@@ -6,8 +6,10 @@ class Images extends Component{
     constructor(){
         super();
         this.state = {
-            imageArray : []
-        }        
+            imageArray : [],
+            activeImageUrl : ''
+        }
+        this.setActiveImage = this.setActiveImage.bind(this);
     }
 
     componentDidMount(){
@@ -24,14 +26,48 @@ class Images extends Component{
         })
     }
 
+    setActiveImage(i,e){
+        console.log(i);
+        this.setState({
+            activeImageUrl : this.state.imageArray[i].secure_url
+        });
+    }
+
     render(){
         return(
-            <div>
+            <div>                
+                <div className="modal fade" id="modalImageView" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">                
+                    <div className="modal-dialog modal-lg" >
+                        <div className="modal-content">                            
+                            <div className="modal-body">
+                                <div className="container-fluid">
+                                    <div className="row">
+                                        <div className="col-12">
+                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="row justify-content-center">
+                                        <div className="col-10 text-center">
+                                            <img src = { this.state.activeImageUrl }  alt = "Yeah not loading" style={{height:'85vh',maxWidth:'100%'}} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                        
+                        </div>
+                    </div>
+                </div>
                 {
-                    this.state.imageArray.map(element => {
+                    this.state.imageArray.map((element,index) => {
                         return(
-                            <div className = "p-2 d-inline-block" style = {{width:'20%'}} key = {element.public_id}>
-                                <img src = { element.secure_url } className = "w-100" />
+                            <div className = "p-2 d-inline-block" 
+                                style = {{width:'20%'}} 
+                                key = {index} 
+                                data-toggle="modal" 
+                                data-target="#modalImageView"                                
+                            >
+                                <img src = { element.secure_url } className = "w-100"  onClick = { (e)=>this.setActiveImage(index,e) } />
                             </div>
                         )
                     })
