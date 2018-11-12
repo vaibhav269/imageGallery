3dcbd8c861cce66924d19707289377244c3856db
Vaibhav <vaibhav.verma2697@gmail.com>
2018-11-12 20:51:14 +0530

    fix db url

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'url':'mongodb://localhost/ImageGallery'
-    //'url':'mongodb://vaibhav:vaibhav2697@ds151453.mlab.com:51453/image-gallery'    
+    //'url':'mongodb://localhost/ImageGallery'
+    'url':'mongodb://vaibhav:vaibhav2697@ds151453.mlab.com:51453/image-gallery'    
 }   
